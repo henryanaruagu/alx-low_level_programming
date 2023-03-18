0dcd511bbce59144b02b1cbc0bd71427390c9bd4
henryanaruagu <henry.anaruagu@hotmail.com>
2023-03-18 15:21:16 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-fput("and that piece of art is \" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is \" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
 
