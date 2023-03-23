6caf6885740a4bc0b8db0a31278232a4c37c1f9a
henryanaruagu <henry.anaruagu@hotmail.com>
2023-03-22 17:36:35 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char = ty;
+for (ty = 'a'; ty <= 'z'; ty++)
+_putchar(ty);
+_putchar('\n');
+return (0);
+}
